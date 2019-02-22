6580bfc30e8ec98c5c234faa364bc95b1c6b95b0
Koichi Akabe <vbkaisetsu@gmail.com>
2019-02-22 16:58:11 +0900

    pypi-test

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,7 +3,7 @@ set -xe
 
 pip install twine
 if [ "${BINARY_PACKAGE}" = "yes" ]; then
-  twine upload -u "${PYPI_USERNAME}" -p "${PYPI_PASSWORD}" $TRAVIS_BUILD_DIR/wheelhouse/primitiv-*.whl;
+  twine upload --repository-url https://test.pypi.org/legacy/ -u "${PYPI_USERNAME}" -p "${PYPI_PASSWORD}" $TRAVIS_BUILD_DIR/wheelhouse/primitiv-*.whl;
 else
-  twine upload -u "${PYPI_USERNAME}" -p "${PYPI_PASSWORD}" $TRAVIS_BUILD_DIR/dist/primitiv-*.tar.gz;
+  twine upload --repository-url https://test.pypi.org/legacy/ -u "${PYPI_USERNAME}" -p "${PYPI_PASSWORD}" $TRAVIS_BUILD_DIR/dist/primitiv-*.tar.gz;
 fi
